d2742fc151473db1961ddf80d4e00b21442aa78e
Mamy-cloud <mamycompteprofessionnel@gmail.com>
2026-02-05 14:10:36 +0300

    requirement

--- fast_food_gasy/settings.py
+++ fast_food_gasy/settings.py
@@ -152,4 +152,34 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+#-----------------railway db-----------------
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+#----------------------supabase---------------------------------------
+INSTALLED_APPS += ["storages"]
+
+DEFAULT_FILE_STORAGE = "monprojet.storages.SupabaseMediaStorage"
+
+AWS_ACCESS_KEY_ID = os.environ["SUPABASE_ACCESS_KEY"]
+AWS_SECRET_ACCESS_KEY = os.environ["SUPABASE_SECRET_KEY"]
+
+AWS_STORAGE_BUCKET_NAME = "media"
+AWS_S3_ENDPOINT_URL = "https://dhnkmismrjgrmqnmqfbf.supabase.co/storage/v1/s3"
+AWS_S3_REGION_NAME = "us-east-1"
+
+MEDIA_URL = "https://dhnkmismrjgrmqnmqfbf.supabase.co/storage/v1/object/public/media/"
+
+
 
