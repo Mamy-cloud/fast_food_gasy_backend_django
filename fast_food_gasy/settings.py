530f9486e382229cffd996aa1369bebe64dd1469
Mamy-cloud <mamycompteprofessionnel@gmail.com>
2026-02-10 17:52:43 +0300

    maj cors allow true

--- fast_food_gasy/settings.py
+++ fast_food_gasy/settings.py
@@ -75,10 +75,7 @@ TEMPLATES = [
 
 
 WSGI_APPLICATION = 'fast_food_gasy.wsgi.application'
-CORS_ALLOW_ALL_ORIGINS = [
-     "http://localhost:5173",
-     "https://fastfoodgasyvuetify2.vercel.app/",
- ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
